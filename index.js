eef9de9f6f250531fac15123de12b503a0e76585
Diego Leite <diegojleite@gmail.com>
2018-09-18 18:03:30 -0300

    Create awesome split function

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+function awesomeSplit(str) {
+  let temp = '';
+  const output = [];
+
+  for(let i=0; i < str.length; i++) {
+    if (str[i] === ' ' || str[i] === '\n' || str[i] == '\t') {
+      output.push(temp);
+      temp = '';
+    } else {
+      temp = temp + str[i];
+    }
+  }
+
+  return output;
+}
+
+fs.readFile('program.pr', 'utf8', function(error, data) {
+  const result = awesomeSplit(data)
+  console.log(result);
+});
+
