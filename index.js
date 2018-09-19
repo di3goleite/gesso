29055cd675ca7d5b654e1ca4ecc77f481b185360
Diego Leite <diegojleite@gmail.com>
2018-09-18 22:08:55 -0300

    Special condition working

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const fs = require('fs');
 
 function awesomeSplit(str) {
   let temp = '';
+  let specialCondition = false;
   const output = [];
 
   for(let i=0; i < str.length; i++) {
-    if (str[i] === ' ' || str[i] === '\n' || str[i] == '\t') {
-      output.push(temp);
+    if (str[i] === '-') {
+      specialCondition = true;
+      temp = temp + str[i];
+    } else if (specialCondition) {
+      if (str[i] === ';') {
+        output.push(temp);
+        output.push(str[i]);
+
+        specialCondition = false;
+        temp = '';
+      } else {
+        temp = temp + str[i];
+      }
+    } else if (str[i] === ' ' || str[i] === '\n' || str[i] === '\t') {
+      if (temp !== '' && temp !== ' ' && temp !== '\n' && temp !== '\t') {
+        output.push(temp);
+      }
+
       temp = '';
     } else {
       temp = temp + str[i];
@@ -17,7 +34,16 @@ function awesomeSplit(str) {
 }
 
 fs.readFile('./teste/program.pr', 'utf8', function(error, data) {
+  console.log('input ====================');
+  console.log(data);
+
+  console.log('characters ====================');
+  console.log(data.split(''));
+  console.log('');
+
   const result = awesomeSplit(data)
+
+  console.log('ouput ====================');
   console.log(result);
 });
 
