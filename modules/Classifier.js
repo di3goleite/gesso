db33b3529a6d073c1a9ff78c83749b2b9f134050
brunomlima11 <brunomenezes253@gmail.com>
2018-09-26 23:40:05 -0300

    comentários do classificador

--- modules/Classifier.js
+++ modules/Classifier.js
@@ -1,3 +1,4 @@
+// Object containing all the regexes for each of the classifications
 const regexes = {
   identifier: /^([a-z]|[A-Z]){1}([a-z]|[A-Z]|[0-9]|_)*/,
   reservedWord: /(^class$|^const$|^variables$|^method$|^return$|^main$|^if$|^then$|^else$|^while$|^read$|^write$|^void$|^int$|^float$|^bool$|^string$|^true$|^false$|^extends$)/,
@@ -12,12 +13,16 @@ const regexes = {
 }
 
 function classify (word) {
+  // Array for the regexes values
   const values = Object.values(regexes);
+  // Array for what classification they represent (organized by the same index)
   const keys = Object.keys(regexes);
   let output = "";
 
+  // Iterate each of the regexes searching for a match
   values.forEach((regex, index) => {
     if(regex.test(word)) {
+      // Assing the output with the classification at the correct index
       output = keys[index];
     }
   });
