fa8abb51c54b2d4372fc9e3a206f6f8f8c98d859
Diego Leite <diegojleite@gmail.com>
2018-09-29 13:22:33 -0300

    Added reserved words list

--- common/lexicalStructure.js
+++ common/lexicalStructure.js
@@ -1,4 +1,5 @@
 const categories = {
+  RESERVED_WORDS: ['class', 'const', 'variables', 'method', 'return', 'main', 'if', 'then', 'else', 'while', 'read', 'write', 'void', 'int', 'float', 'bool', 'string', 'true', 'false', 'extends'],
   ARITHMETIC_OPERATORS: ['\\+', '\\-', '\\*', '\\/', '\\+\\+', '\\-\\-'],
   RELATIONAL_OPERATORS: ['!=', '==', '<', '<=', '>', '>=', '='],
   LOGIC_OPERATORS: ['\\!', '\\&\\&', '\\|\\|'],
