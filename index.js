d933a1f6ec590ee26ffeff8fed4b32d64a7b4658
Diego Leite <diegojleite@gmail.com>
2018-09-29 11:58:35 -0300

    Clean up function working well

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 
 // Lib imports
-const split = require('./lib/split');
+const lexicalAnalyzer = require('./lib/lexicalAnalyzer');
 const classifier = require('./lib/classifier');
 
 fs.readFile('./teste/program.pr', 'utf8', function(error, data) {
   console.log('input ====================');
   console.log(data);
+  console.log('');
 
-  // console.log('characters ====================');
-  // console.log(data.split(''));
-  // console.log('');
+  let result = lexicalAnalyzer.cleanUp(data);
+  result = lexicalAnalyzer.split(result);
 
   console.log('ouput ====================');
-  const result = split.run(data);
   console.log(result);
-  console.log(result.length);
   console.log('');
 
   // console.log('classification ====================');
