d489bcee2382d52b8495e7cc44c998beb0a5f744
Diego Leite <diegojleite@gmail.com>
2018-09-29 08:31:32 -0300

    Split function working

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ fs.readFile('./teste/program.pr', 'utf8', function(error, data) {
   console.log('input ====================');
   console.log(data);
 
-  console.log('characters ====================');
-  console.log(data.split(''));
-  console.log('');
+  // console.log('characters ====================');
+  // console.log(data.split(''));
+  // console.log('');
 
   console.log('ouput ====================');
   const result = split.run(data);
   console.log(result);
+  console.log(result.length);
   console.log('');
 
-  console.log('classification ====================');
-  const classifications = classifier.run(result);
-  console.log(classifications);
+  // console.log('classification ====================');
+  // const classifications = classifier.run(result);
+  // console.log(classifications);
 });
