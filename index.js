5bcdc55393ee1a06a72b14fad152aea666463cff
brunomlima11 <brunomenezes253@gmail.com>
2018-09-30 14:10:47 -0300

    Escrevendo saída do analisador em arquivo

--- index.js
+++ index.js
@@ -20,4 +20,29 @@ fs.readFile('./teste/program.pr', 'utf8', function(error, data) {
   const classifications = classifier.run(result);
   console.log('classification ====================');
   console.log(classifications);
+
+  let stream = fs.createWriteStream("teste/output.txt");
+  stream.once("open", fd => {
+    const map = require("./common/classificationMap").map;
+    const mapKeys = Object.keys(map);
+    const mapValues = Object.values(map);
+
+    // Number of Errors found in analysis
+    let err = 0;
+    classifications.forEach(obj => {
+      // Counting errors
+      if(obj.type == "error") err++;
+
+      // Mapping classification to class abreviation
+      let position = mapKeys.indexOf(obj.class);
+      let mappedClass = mapValues[position]
+
+      stream.write(obj.line + "\t" + mappedClass + "\t" + obj.value + "\n"); 
+    })
+
+    if(err === 0) stream.write("\n\nArquivo analisado com sucesso. Nenhum erro foi encontrado");
+    else stream.write("\n\nArquivo analisado com falhas. " + err + " erros foram encontrados.")
+    
+    stream.end();
+  })
 });
