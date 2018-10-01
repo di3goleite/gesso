547c36696e851e6ced7f2f9dda1cb046cb4b3f96
brunomlima11 <brunomenezes253@gmail.com>
2018-09-30 23:36:25 -0300

    escrevendo a saída na formatação adequada

--- index.js
+++ index.js
@@ -29,20 +29,33 @@ fs.readFile('./teste/program.pr', 'utf8', function(error, data) {
 
     // Number of Errors found in analysis
     let err = 0;
-    classifications.forEach(obj => {
-      // Counting errors
-      if(obj.type == "error") err++;
+    let errors = [];
 
+    classifications.forEach(obj => {
       // Mapping classification to class abreviation
       let position = mapKeys.indexOf(obj.class);
-      let mappedClass = mapValues[position]
+      let mappedClass = mapValues[position];
 
-      stream.write(obj.line + "\t" + mappedClass + "\t" + obj.value + "\n"); 
+      let line = obj.line;
+      if(obj.line < 10) line = "0" + obj.line;
+      
+      let writeData = line + "\t" + mappedClass + "\t" + obj.value + "\n";
+      if(obj.type == "error") {
+        // Counting errors
+        err++;
+        // Push on array to be written on the end of the file
+        errors.push(writeData);
+      }
+      else stream.write(writeData); 
     })
 
     if(err === 0) stream.write("\n\nArquivo analisado com sucesso. Nenhum erro foi encontrado");
-    else stream.write("\n\nArquivo analisado com falhas. " + err + " erros foram encontrados.")
-
+    else {
+      stream.write("\nArquivo analisado com falhas. " + err + " erros foram encontrados.\n\n")
+      errors.forEach(obj => {
+        stream.write(obj);
+      })
+    }
     stream.end();
   });
 });
